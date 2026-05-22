a45a5b53af004accb73717dca3ddf5b94390895f
Dmitry globa <dima.globa2015@yandex.ru>
2026-05-22 18:42:43 +0800

    models

--- core/urls.py
+++ core/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("users/", include("users.urls", namespace="users")),
     path("clients/", include("clients.urls", namespace="clients")),
+    path("mailings/", include("mailings.urls", namespace="mailings")),
 ]
--- mailings/views.py
+++ mailings/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import (
+    ListView,
+)
 
-# Create your views here.
+from .models import Message
+
+
+class MessagetListView(LoginRequiredMixin, ListView):
+    """Просмотр списка писим."""
+
+    model = Message
+    template_name = "malings/message_list.html"
+    context_object_name = "messages"
+
+    def get_queryset(self):
+        """Возвращает только те письма, которые принадлежат текущему пользователю."""
+        # Менеджеры могут просматривать вообще все сообщения
+        if self.request.user.groups.filter(name="Менеджеры").exists():
+            return Message.objects.all()
+        return Message.objects.filter(owner=self.request.user)
