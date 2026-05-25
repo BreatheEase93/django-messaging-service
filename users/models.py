4deac5a3aca10a62aeccf54cdae1f24a01bf7016
Dmitry globa <dima.globa2015@yandex.ru>
2026-05-25 10:22:10 +0800

    fix user: add Meta

--- users/models.py
+++ users/models.py
@@ -20,3 +20,7 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.email
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
